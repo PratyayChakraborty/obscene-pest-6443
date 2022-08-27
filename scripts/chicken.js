f5e7c9a2eb3f9cfeb4ca4b07329832a567c2c62c
hrehankhan <rk46977@gmail.com>
2022-08-27 19:58:58 +0530

    added

---
+++ scripts/chicken.js
@@ -0,0 +1,350 @@
+// var chikendata = JSON.parse(localStorage.getItem("chikendata")) || [];
+
+var chikendata = [
+  {
+    imgUrl:
+      "https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_5785b9065d7e1/11/prod_image/1584770284.9151--2020-03-2111:28:04--738?format=webp",
+    name: "Chicken Curry Cut (Small Pcs)",
+    des: "A combination of dark and white meat, the Chicken Curry Cut",
+    net_tag: "Net wt:",
+    net: "500",
+    gross_tag: "Gross:",
+    gross: "526",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "169",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+    bike: "https://www.licious.in/img/rebranding/express_delivery.svg",
+    delv: "Today in 90 min",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_3nqjtznab5c/11/prod_image/1634534931.0234--2021-10-1810:58:51--1818?format=webp",
+    name: "Chicken Breast - Boneless",
+    des: "Chicken Breast is one of the meatier cuts of a Chicken, ",
+    net_tag: "Net wt:",
+    net: "450",
+    gross_tag: "Gross:",
+    gross: "500",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "285",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+    {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_av4kgtb3l1h/11/prod_image/1631077952.5746--2021-09-0810:42:32--1818",
+
+    name: "Chicken Curry Cut (Small Pcs)",
+    des: "A combination of dark and white meat ",
+    net_tag: "Net wt:",
+    net: "1000",
+    gross_tag: "Gross:",
+    gross: "1200",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "335",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_57234c427648b/11/prod_image/1574674666.5753--2019-11-2515:07:46--539?format=webp",
+
+    name:"Chicken Curry Cut (Large Pcs)",
+    des:"A mix of dark and white meat, our large Chicken Curry",
+    net_tag: "Net wt:",
+    net: "500",
+    gross_tag: "Gross:",
+    gross: "526",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "169",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_id9jjmp0h34/11/prod_image/1574677401.9946--2019-11-2515:53:21--539?format=webp",
+
+    name: "Chicken Drumstick",
+    des:"Our skinless and bone-in Chicken Drumsticks are cut from the",
+
+    net_tag: "Net wt:",
+    net: "500",
+    gross_tag: "Gross:",
+    gross: "625",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "255",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_fhijtqu0u5q/11/prod_image/1634537661.8768--2021-10-1811:44:21--1818?format=webp",
+
+    name: "Chicken Mini Bites (Boneless)",
+    des: "The Chicken Mini Bites from Licious are finely cut, boneless",
+
+    net_tag: "Net wt:",
+    net: "250",
+    gross_tag: "Gross:",
+    gross: "350",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "185",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_3nqjtzosblz/11/prod_image/1634536846.0889--2021-10-1811:30:46--1818?format=webp",
+
+    name:"Chicken Thigh (Boneless)",
+    des:"Cut from the upper-leg, above the knee joint, Chicken T",
+    net_tag: "Net wt:",
+    net: "450",
+    gross_tag: "Gross:",
+    gross: "565",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "285",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_fhijtzoq66l/11/prod_image/1634537152.6999--2021-10-1811:35:52--1818?format=webp",
+
+    name:"Chicken Mince - Keema",
+    des: "Chicken Mince consists of Chicken Breast Fillets ground to p",
+    net_tag: "Net wt:",
+    net: "450",
+    gross_tag: "Gross:",
+    gross: "564",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "305",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_fmhkgtbdprx/11/prod_image/1631077251.0632--2021-09-0810:30:51--1818",
+
+    name: "Chicken Breast Boneless",
+    des: "Chicken Breast is one of the meatier cuts of a Chicken,....",
+
+    net_tag: "Net wt:",
+    net: "900",
+    gross_tag: "Gross:",
+    gross: "1200",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "525",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  }, 
+  {
+    imgUrl: "https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_ejejbf09h51/11/prod_image/1574096124.4189--2019-11-1822:25:24--458?format=webp",
+
+    name:"Chicken Lollipop",
+    des:  "Chicken Lollipops at Licious are expertly cleaned, cut and trimmed. ",
+
+    net_tag: "Net wt:",
+    net: "10",
+    gross_tag: "Gross:",
+    gross: "10",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "195",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  }, 
+  
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_7kyknomrcdb/11/prod_image/1620412273.7916--2021-05-0800:01:13--738",
+
+    name:"Chicken Tangdi - Biryani Cut",
+    des:"If you're a fan of leg pieces in your chicken dishes, this cut is for you! ",
+
+    net_tag: "Net wt:",
+    net: "550",
+    gross_tag: "Gross:",
+    gross: "545",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "255",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+  {
+    imgUrl:"https://d2407na1z3fc0t.cloudfront.net/prodDev/pr_57234c427648b/11/prod_image/1574674666.5753--2019-11-2515:07:46--539?format=webp",
+
+    name:"Chicken Curry Cut (Large Pcs)",
+    des:"A mix of dark and white meat, our large Chicken Curry",
+    net_tag: "Net wt:",
+    net: "500",
+    gross_tag: "Gross:",
+    gross: "526",
+    unit: "gms",
+    price_tag: "MRP:",
+    cuurency: "₹",
+    price: "169",
+    strikePrice: "239",
+    off: "15",
+    off_tag: "%OFF",
+  },
+];
+
+  // localStorage.setItem("chikendata",JSON.stringify(chikendata));
+  
+
+
+console.log("chikendata", chikendata);
+chikendata.map(function (e, index, array) {
+  var main = document.createElement("div");
+  main.setAttribute("class", "main");
+  var img = document.createElement("img");
+  img.setAttribute("src", e.imgUrl);
+  img.setAttribute("class", "image");
+
+  var name = document.createElement("h4");
+  name.innerText = e.name;
+  var des = document.createElement("p");
+  des.setAttribute("class", "des");
+  des.innerText = e.des;
+  var wgt = document.createElement("div");
+  wgt.setAttribute("class", "wgt");
+  var net_tag = document.createElement("h6");
+  net_tag.innerText = e.net_tag;
+
+  var net = document.createElement("h6");
+  net.innerText = e.net;
+  var n_gm = document.createElement("h6");
+  n_gm.innerText = e.unit;
+
+  var gross_tag = document.createElement("h6");
+  gross_tag.innerText = e.gross_tag;
+  var gross = document.createElement("h6");
+  gross.innerText = e.gross;
+  var g_gm = document.createElement("h6");
+  g_gm.innerText = e.unit;
+
+  var hold = document.createElement("div");
+    hold.setAttribute("class", "hold");
+    
+  var pri = document.createElement("div");
+  pri.setAttribute("class", "pri");
+  var price_tag = document.createElement("h5");
+  price_tag.innerText = e.price_tag;
+  var cuurency = document.createElement("h5");
+  cuurency.innerText = e.cuurency;
+  var price = document.createElement("h5");
+  price.innerText = e.price;
+  var strikePrice = document.createElement("strike");
+  strikePrice.innerText = e.strikePrice;
+//   var off = document.createElement("h5");
+//   off.innerText = e.off;
+ 
+//   var bike = document.createElement("img");
+//   bike.setAttribute("src", e.bike);
+//   bike.setAttribute("class", "image");
+
+  var btndiv = document.createElement("div");
+  btndiv.setAttribute("class", "btndiv");
+  var addCart = document.createElement("button");
+  addCart.setAttribute("class", "addcartbtn");
+  addCart.innerHTML = "Add to Cart";
+  addCart.addEventListener("click", function () {
+    addToCart(e);
+});
+  wgt.append(net_tag, net, n_gm, gross_tag, gross, g_gm);
+  pri.append(price_tag, cuurency, price, strikePrice, addCart);
+  // btndiv.append(addToCart)
+
+  main.append(img, name, des, wgt, pri);
+  document.getElementById("container").append(main);
+
+  // console.log(e);
+});
+
+
+let cartdata = [];
+
+function addToCart(e){
+  cartdata.push(e)
+  localStorage.setItem("cartdata", JSON.stringify(cartdata));
+  console.log(cartdata);
+    };
+    
+
+    //  cartdata = JSON.parse(localStorage.getItem("cartdata")) || [];
+    // var placeadd = localStorage.getItem("placeadd") || " ";
+    
+
+
+    // let quntarr = JSON.parse(localStorage.getItem("quntarr")) || [];
+
+    // function addproducttocart(){
+    //   let total = 0;
+
+      // let item = ``
+      // cartdata.forEach((el,ind) => {
+      //   quntarr.push(1)
+      //   localStorage.setItem("quntarr",JSON.stringify(quntarr));
+        // item += `
+        //   <div id="qut">
+        //     <div id="add-qut" onclick="addqun(${quntarr,ind})" class="smoke">+</div>
+        //     <div id="item-qut">${quntarr[ind]}</div>
+        //     <div id="sub-qut" onclick="subqun(${quntarr,ind})" class="smoke">-</div>
+        //   </div>
+        // `
+
+        // summary.innerHTML=item;
+        // document.getElementById("add-qut").addEventListener("click",(quntarr) => {
+        //   quntarr[ind]++;
+
+        //   console.log(quntarr[ind]);
+        // });
+      // });
+    // };
+
+// function addqunt(ind) {
+//   quntarr[ind]++;
+
+//   localStorage.setitem("quntarr",JSON.stringify(quntarr));
+//   slidorder()
+// }
+
+// function subqunt(ind) {
+//   quntarr[ind]--;
+
+//   localStorage.setitem("quntarr",JSON.stringify(quntarr));
+//   if(quntarr[ind]==0){delpro(ind)}
+//   slidorder()
+// }
+
+// function delpro(ind) {
+//   cartdata.splice(ind,1);
+//   quntarr.splice(ind,1);
+//   slideorder();
+// }
